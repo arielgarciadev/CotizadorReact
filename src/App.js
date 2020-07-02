df76d9e56fd69953bbb73b8e69feae57a21d029c
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-07-02 12:17:28 -0300

    Creación de contenedores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled'
 import Header from './components/Header'
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3em;
+`
 
 const App = () => {
   return (
-    <Header 
-      titulo='Cotizador de Seguros'    
-    />
+    <Contenedor>
+      <Header 
+        titulo='Cotizador de Seguros'    
+      />
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+    </Contenedor>
   )
 }
 
